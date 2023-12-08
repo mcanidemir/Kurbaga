ec2ed5d85ae965cef3f400e4d842d2139a026b97
Bugraunluu <bugraunl0607@gmail.com>
2023-12-09 00:55:23 +0300

    a

--- Assets/codes/Enemy.cs
+++ Assets/codes/Enemy.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public GameObject health_bar;
     public int maxHealth = 100;
-    int currentHealth;
+    float currentHealth;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -27,5 +29,10 @@ public class Enemy : MonoBehaviour
     public void Die()
     {
         Destroy(gameObject);
+    }
+    private void Update()
+    {
+        health_bar.transform.localScale = new Vector3(0.35f*currentHealth/100,0.03f , 1);
+
     }
 }
