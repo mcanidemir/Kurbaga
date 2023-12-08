7633009d41b37772e23bc69c4808d9ec1ffab575
Bugraunluu <bugraunl0607@gmail.com>
2023-12-08 18:55:41 +0300

    öl öl seni lanet

---
+++ Assets/codes/Player_Move.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Move : MonoBehaviour
+{
+    public float speed;
+    public float jump;
+
+    private float Move;
+
+    public Rigidbody2D rb;
+
+    public bool isJumping;
+
+    //public Animator animator;
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        Move = Input.GetAxis("Horizontal");
+
+        //animator.SetFloat("Speed", Mathf.Abs(Move));
+        rb.velocity = new Vector2(speed * Move, rb.velocity.y);
+
+        if (Input.GetButtonDown("Jump") && isJumping == false)
+        {
+            rb.AddForce(new Vector2(rb.velocity.x, jump));
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Floor"))
+        {
+            isJumping = false;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Floor"))
+        {
+            isJumping = true;
+        }
+    }
+}
